e7580ac59d5e22bcdf42052ab1c2388c7d779621
Mateusz Kolodziejczyk <20084190@mail.wit.ie>
2022-02-11 02:03:24 +0000

    Add visible movement range

---
+++ Assets/Scripts/Helper/DistanceHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Helper
+{
+    public class DistanceHelpers
+    {
+        public static int Vector3IntManhattanDistance(Vector3Int v1, Vector3Int v2)
+        {
+            return Math.Abs(v1.x - v2.x) +
+                   Math.Abs(v1.y - v2.y);
+        }
+    }
+}
--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using Character;
+using Helper;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -19,6 +21,10 @@ namespace Movement
         private CharacterData characterData;
 
         private PlayerTurn playerTurn;
+
+        private bool isShowingMovementRange = false;
+
+        private HashSet<Vector3Int> movableSquares = new HashSet<Vector3Int>();
         // Start is called before the first frame update
         void Start()
         {
@@ -33,10 +39,18 @@ namespace Movement
         void Update()
         {
 
+
             if (!playerTurn.IsPlayerTurn())
             {
                 return;
             }
+            
+            if (isShowingMovementRange == false)
+            {
+                ShowMovementRange();
+                isShowingMovementRange = true;
+            }
+            
             if (Input.GetMouseButtonDown(0))
             {
                 UpdateCurrentCellMouse();
@@ -57,8 +71,7 @@ namespace Movement
                 return;
             }
             // Get the manhattan distance to calculate where the player can move
-            int distance = Math.Abs(characterData.Position.x - gridPos.x) +
-                           Math.Abs(characterData.Position.y - gridPos.y);
+            int distance = DistanceHelpers.Vector3IntManhattanDistance(characterData.Position, gridPos);
             if (distance > characterData.MovementSpeed)
             {
                 return;
@@ -68,13 +81,70 @@ namespace Movement
             Debug.Log(gridPos.x + "," + gridPos.y);
             var newPos = tilemap.CellToLocal(gridPos);
             transform.position = newPos;
-            Debug.Log(tilemap.size);
+            
+            // Cleanup the movement range visual
+            CleanupMovementRange();
+            isShowingMovementRange = false;
         }
  
         void UpdateCurrentCell(Vector3Int offset)
         {
             characterData.Position += offset;
             transform.position = tilemap.CellToLocal(characterData.Position);
+        }
+
+        private void ShowMovementRange()
+        {
+            var startPos = characterData.Position;
+            // Color moveable tiles black
+
+            // Start a breadth first search starting from the player pos and ending when movement speed is expended.
+            var visited = new HashSet<Vector3Int>();
+            var queue = new Queue<Vector3Int>();
+            queue.Enqueue(startPos);
+            
+            // This list contains the vectors that are added to the current position to get its four directly adjacent squares.
+            var adjacentAddition = new List<Vector3Int>() { 
+                new(1,0, 0), 
+                new(-1, 0, 0), 
+                new(0, 1, 0), 
+                new(0, -1, 0)
+            };
+            
+            while (queue.Count > 0)
+            {
+                var pos = queue.Dequeue();
+                if (!visited.Contains(pos))
+                {
+                    // If current node's distance to the start is the same as movement speed, return
+                    if (DistanceHelpers.Vector3IntManhattanDistance(pos, startPos) >= characterData.MovementSpeed)
+                    {
+                        return;
+                    }
+                    visited.Add(pos);
+                    foreach (var adjacentAdd in adjacentAddition)
+                    {
+                        var adjacent = adjacentAdd + pos;
+                        if (tilemap.HasTile(adjacent))
+                        {
+                            tilemap.SetTileFlags(adjacent, TileFlags.None);
+                            tilemap.SetColor(adjacent, Color.magenta);
+                            movableSquares.Add(adjacent);
+                            queue.Enqueue(adjacent);
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        private void CleanupMovementRange()
+        {
+            foreach (var movableSquare in movableSquares)
+            {
+                tilemap.SetColor(movableSquare, Color.white);
+            }
+            movableSquares.Clear();
         }
     }
 }
